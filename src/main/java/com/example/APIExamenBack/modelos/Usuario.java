767920ecbb2e8ac78740a1d83c576f79989a5a24
diaz8219 <delta.82198@gmail.com>
2025-08-13 14:25:13 -0500

    Añadir Getters y Setters en Usuario.java

--- src/main/java/com/example/APIExamenBack/modelos/Usuario.java
+++ src/main/java/com/example/APIExamenBack/modelos/Usuario.java
@@ -37,4 +37,52 @@ public class Usuario {
         this.telefono = telefono;
         this.tipoUsuario = tipoUsuario;
     }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public TipoUsuario getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(TipoUsuario tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
 }
